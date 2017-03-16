a6eaff0fb5f2d12e42087d1b524496ea34b6a238
ping <lastmodified@gmail.com>
2017-03-16 19:04:38 +0800

    Bump version to 0.1.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except ImportError:
     from distutils.core import setup
 
 __author__ = 'ping <lastmodified@gmail.com>'
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 packages = [
     'instagram_private_api_extensions'
