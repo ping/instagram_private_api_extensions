b165e51d1704ab758e94a1523bc209d59f98d82c
ping <lastmodified@gmail.com>
2017-03-17 21:00:08 +0800

    Change prepare_image tests to use local media

--- tests/test_media.py
+++ tests/test_media.py
@@ -12,25 +12,27 @@ except ImportError:
 
 class TestMedia(unittest.TestCase):
 
+    TEST_IMAGE_PATH = 'media/test.jpg'
+    TEST_IMAGE_SIZE = (640, 493)
     TEST_VIDEO_PATH = 'media/test.mp4'
     TEST_VIDEO_SIZE = (640, 360)
     TEST_VIDEO_DURATION = 60.0
 
     def test_prepare_image(self):
         _, size = media.prepare_image(
-            'http://i.imgur.com/buaMomp.jpg', max_size=(400, 400), aspect_ratios=0.8)
+            self.TEST_IMAGE_PATH, max_size=(400, 400), aspect_ratios=0.8)
         self.assertLessEqual(size[0], 400, 'Invalid width.')
         self.assertLessEqual(size[1], 400, 'Invalid height.')
-        self.assertEqual(1.0 * size[0] / size[1], 0.8, 'Invalid aspect ratio.')
+        self.assertEqual(round(1.0 * size[0] / size[1], 2), 0.8, 'Invalid aspect ratio.')
 
     def test_prepare_image2(self):
         _, size = media.prepare_image(
-            'http://i.imgur.com/buaMomp.jpg', max_size=(400, 350), aspect_ratios=(0.8, 1.2))
+            self.TEST_IMAGE_PATH, max_size=(400, 350), aspect_ratios=(0.8, 1.2))
         self.assertLessEqual(size[0], 400, 'Invalid width.')
         self.assertLessEqual(size[1], 350, 'Invalid height.')
         ar = 1.0 * size[0] / size[1]
-        self.assertLessEqual(ar, 1.2)
-        self.assertGreaterEqual(ar, 0.8)
+        self.assertLessEqual(round(ar, 2), 1.2)
+        self.assertGreaterEqual(round(ar, 2), 0.8)
 
     def test_prepare_video(self):
         _, size, duration, _ = media.prepare_video(
